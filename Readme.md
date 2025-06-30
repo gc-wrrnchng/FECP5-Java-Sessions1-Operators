0b299d210df3fb29a791873a35060270def5531c
Warren Denzel Cheng <warrendenzel.cheng@gfitftlapt00849.ad.mynt.xyz>
2025-06-30 15:01:40 +0800

    Final Submission, Celsius to Fahrenheit Converter

---
+++ Readme.md
@@ -0,0 +1,9 @@
+## Warren Denzel F. Cheng
+
+# Lab 1: Celsius to Fahrenheit Converter
+
+Sample Output 1:
+![img.png](img.png)
+
+Sample Output 2:
+![img_1.png](img_1.png)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,17 @@ import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        // Get Celsius input
+        System.out.println("Enter temperature in Celsius: ");
+        float getCelsius = scanner.nextFloat();
+
+        // Convert Celsius to Fahrenheit
+        float getFahrenheit = (getCelsius * ((float) 9/5)) + 32;
+
+        // Display Fahrenheit
+        System.out.print("Temperature in Fahrenheit: ");
+        System.out.printf("%.2f", getFahrenheit);
     }
 }
